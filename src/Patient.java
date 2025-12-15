ed1b1f2b94aab5a10987205ee19fba21b068df74
miesz <mieszalontko0@o2.pl>
2025-12-15 10:45:53 +0100

    Pateint class

---
+++ src/Patient.java
@@ -0,0 +1,8 @@
+import java.io.Serializable;
+
+public class Patient extends Person implements Treatable {
+    private String condition;
+    public void recieveTreatment(){
+
+    }
+}
--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,6 @@ public abstract class Person {
     private String name;
     private int age;
     public void introduce() {
-        System.out.println("I am " + name);
+        System.out.println(name+", "+age);
     }
 }
